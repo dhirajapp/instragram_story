f1d18648f7ad0cc9c8250f09413ed928a262c711
Dhiraj Kumar <dhirajkumar31298@gmail.com>
2021-03-28 22:33:43 +0530

    nginx

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+function getIp(req) {
+  let ip = req.connection.remoteAddress;
+  ip = ip.replace('::ffff:', '');
+
+  if (ip == '127.0.0.1') {
+    ip = req.headers['x-real-ip'];
+  }
+
+  return ip;
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World from: ' + getIp(req));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
